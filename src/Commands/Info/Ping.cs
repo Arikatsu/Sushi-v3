03211dcc7f074e89289a98ef30b52cc1af6975df
Arikatsu <tanmaisingh9559@gmail.com>
2023-09-12 00:13:36 +0530

    Ping command

---
+++ src/Commands/Info/Ping.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Interactions;
+
+namespace Sushi.Commands.Info
+{
+    public class PingModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        [SlashCommand("ping", "Get the bot's ping")]
+        public async Task HandlePingAsync()
+        {
+            await DeferAsync();
+            await FollowupAsync($"Pong! {GlobalVars.DiscordClient.Latency}ms");
+        }
+    }
+}
