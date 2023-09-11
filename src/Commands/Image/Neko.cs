8621ad3ec6bfce15776a44f737be9a654fe44488
Arikatsu <tanmaisingh9559@gmail.com>
2023-09-11 18:20:03 +0530

    Neko command

---
+++ src/Commands/Image/Neko.cs
@@ -0,0 +1,28 @@
+﻿using Discord;
+using Discord.Interactions;
+using Newtonsoft.Json.Linq;
+
+namespace Sushi.Commands.Image
+{
+    internal class NekoModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        [SlashCommand("neko", "Fetches a random cat girl image")]
+        public async Task HandleNekoAsync()
+        {
+            await DeferAsync();
+
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("http://api.waifu.pics/sfw/neko");
+            var content = await response.Content.ReadAsStringAsync();
+
+            JObject nekoJson = JObject.Parse(content);
+
+            Embed embed = new EmbedBuilder()
+                .WithColor(Color.DarkGrey)
+                .WithImageUrl(nekoJson?["url"]?.ToString())
+                .Build();
+
+            await FollowupAsync(embeds: new[] { embed });
+        }
+    }
+}
