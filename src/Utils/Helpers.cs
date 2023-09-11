b36f6149e791e25e1b2565f563a7d44f6739ad9c
Arikatsu <tanmaisingh9559@gmail.com>
2023-09-12 00:12:32 +0530

    Helper class functions

---
+++ src/Utils/Helpers.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace Sushi.Utils
+{
+    internal class Helpers
+    {
+        public static string ConvertUpperCamelCaseToWords(string input)
+        {
+            return Regex.Replace(input, "([A-Z])", " $1", RegexOptions.Compiled).Trim();
+        }
+
+        public static string GetUptime()
+        {
+            TimeSpan uptime = DateTime.Now - Process.GetCurrentProcess().StartTime;
+            return $"{uptime.Days} days, {uptime.Hours} hours, {uptime.Minutes} minutes, {uptime.Seconds} seconds";
+        }
+
+        public static string GetDiscordTimestamp(DateTimeOffset dateTimeOffset)
+        {
+            return $"<t:{dateTimeOffset.ToUnixTimeSeconds()}:F>";
+        }
+    }
+}
