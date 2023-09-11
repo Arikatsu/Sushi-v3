d0799f4e371ab055152947f59a2f283cea6a6a24
Arikatsu <tanmaisingh9559@gmail.com>
2023-09-11 18:19:46 +0530

    Achiement command

---
+++ src/Commands/Image/Achievement.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Interactions;
+
+namespace Sushi.Commands.Image
+{
+    internal class AchievementModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        [SlashCommand("achievement", "Turns text into a Minecraft achievement")]
+        public async Task HandleAchievementAsync([Summary("text", "The text to turn into an achievement")] string text)
+        {
+            await DeferAsync();
+
+            string replacedText = text.Replace(" ", "+");
+
+            await FollowupAsync($"https://minecraftskinstealer.com/achievement/12/Achievement%20Get!/{replacedText}");
+        }
+    }
+}
