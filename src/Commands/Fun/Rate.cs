27f18d4b94d3a50f356893d695f32b51a8fe3980
Arikatsu <tanmaisingh9559@gmail.com>
2023-09-11 18:19:23 +0530

    Rate command group

---
+++ src/Commands/Fun/Rate.cs
@@ -0,0 +1,40 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+
+namespace Sushi.src.Commands.Fun
+{
+    [Group("rate", "Rates various attributes")]
+    public class RateModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        [SlashCommand("pp", "Rates your or someone else's pp size")]
+        public async Task HandlePPAsync([Summary("user", "The user to rate the pp size of")] SocketUser? user = null)
+        { 
+            await DeferAsync();
+
+            bool nullUser = user == null;
+            user ??= Context.User;
+
+            if (user?.Id == 593787701409611776)
+            {
+
+                await FollowupAsync($"{(nullUser ? "Your" : user?.Username + "'s")} dick score is 100/100");
+                return;
+            }
+
+            var ppRate = new Random().Next(0, 100);
+
+            await FollowupAsync($"{(nullUser ? "Your" : user?.Username + "'s")} dick score is {ppRate}/100");
+        }
+
+        [SlashCommand("waifu", "Rates how wife-able you or someone else is are")]
+        public async Task HandleWaifuAsync([Summary("user", "The user to rate how wife-able they are")] SocketUser? user = null)
+        {
+            await DeferAsync();
+
+            var waifuRate = new Random().Next(0, 100);
+
+            await FollowupAsync($"{(user == null ? "You are" : user?.Username + "is")} a {waifuRate}/100 waifu");
+        }
+    }
+}
