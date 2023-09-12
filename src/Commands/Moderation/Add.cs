86c9ea6008e1bf3e98ab651178167eb239a53ebb
Scald <104459145+Arikatsu@users.noreply.github.com>
2023-09-12 21:24:10 +0530

    Finish AddEmoji command

--- src/Commands/Moderation/Add.cs
+++ src/Commands/Moderation/Add.cs
@@ -1,51 +1,59 @@
 ﻿using Discord;
 using Discord.Interactions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Sushi.Utils;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Sushi.Commands.Moderation
 {
     [Group("add", "Various commands for adding something by moderators")]
+    [DefaultMemberPermissions(GuildPermission.ManageEmojisAndStickers)]
     public class Add : InteractionModuleBase<SocketInteractionContext>
     {
-        [SlashCommand("emojis", "Add emojis to the server from other servers")]
-        [DefaultMemberPermissions(GuildPermission.ManageEmojisAndStickers)]
-        public async Task HandleEmojisAsync([Summary("emojis", "Add all your emojis in one sentence")] string emojiString)
+        [SlashCommand("emotes", "Add emojis to the server from other servers")]
+        public async Task HandleEmojisAsync([Summary("emotes", "Add all your emojis in one sentence")] string emoteString)
         {
             await DeferAsync();
+            
+            var guildUser = Context.Guild.GetUser(Context.User.Id);
 
-            if (!Context.Guild.CurrentUser.GuildPermissions.ManageEmojisAndStickers)
+            if (!guildUser.GuildPermissions.ManageEmojisAndStickers ||
+                !guildUser.GuildPermissions.Administrator)
+            {
+                await FollowupAsync("You don't have permission to manage emojis in this server.");
+                return;
+            }
+
+            if (!Context.Guild.CurrentUser.GuildPermissions.ManageEmojisAndStickers ||
+                !Context.Guild.CurrentUser.GuildPermissions.Administrator)
             {
                 await FollowupAsync("I don't have permission to manage emojis in this server.");
                 return;
             }
 
-            Regex regex = new(@"/<?(a)?:?(\w{2,32}):(\d{17,19})>?/gi");
+            List<string> emotes = new();
 
-            MatchCollection matches = regex.Matches(emojiString);
-
-            List<string> emojiNames = new();
-            foreach (Match match in matches.Cast<Match>())
+            foreach (Match match in Regex.Matches(emoteString, @"<a?:\w+:\d+>").Cast<Match>())
             {
-                emojiNames.Add(match.Groups[2].Value);
+                emotes.Add(match.Value);
             }
 
-            await FollowupAsync($"Adding {emojiNames.Count} emojis...");
+            await FollowupAsync($"Adding {emotes.Count} emojis...");
 
-            foreach (string emojiName in emojiNames)
+            foreach (string emote in emotes)
             {
                 try
                 {
-                    var emoji = Emoji.Parse(
-                    await Context.Guild.CreateEmoteAsync(emojiName, new(new Uri($"https://cdn.discordapp.com/emojis/{emojiName}.png")));
+                    Emote emoteParsed = Emote.Parse(emote);
+                    
+                    var stream = await new HttpClient().GetStreamAsync(emoteParsed.Url + "?size=64&quality=lossless");
+
+                    await Context.Guild.CreateEmoteAsync(emoteParsed.Name, new Discord.Image(stream));
+                    await Context.Channel.SendMessageAsync($"Added {emoteParsed}");
                 }
-                catch (Exception)
+                catch (Exception e)
                 {
-                    await Context.Channel.SendMessageAsync($"Failed to add emoji {emojiName}");
+                    Logger.Error(e.Message);
+                    await Context.Channel.SendMessageAsync($"Failed to add emoji {emote}");
                 }
             }
         }
