52d72db03ec0a5bd3a68411c0fc20cea4feeae3d
Scald <104459145+Arikatsu@users.noreply.github.com>
2023-09-16 17:55:01 +0530

    WarnLimit command

---
+++ src/Commands/Moderation/Warn.cs
@@ -0,0 +1,38 @@
+﻿using Discord;
+using Discord.Interactions;
+using MongoDB.Driver;
+using Sushi.Models;
+
+namespace Sushi.Commands.Moderation
+{
+    [Group("warn", "Commands to manage warns of a user for a server.")]
+    public class WarnModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        [SlashCommand("limit", "Sets the minimum number of warns before a user is automatically banned.")]
+        [DefaultMemberPermissions(GuildPermission.BanMembers)]
+        public async Task HandleLimitAsync([Summary("amount", "The minimum amount to be set. Default is already set to 3.")] int limit)
+        {
+            await DeferAsync();
+
+            var collection = GlobalVars.Database.GetCollection<WarnCollection.MaxWarnsObject>("max_warns");
+            var isDocExists = await collection.Find(x => x.GuildId == Context.Guild.Id.ToString()).AnyAsync();
+
+            if (isDocExists == true)
+            {
+                var filter = Builders<WarnCollection.MaxWarnsObject>.Filter.Eq(obj => obj.GuildId, Context.Guild.Id.ToString());
+                var update = Builders<WarnCollection.MaxWarnsObject>.Update.Set(obj => obj.MaxWarns, limit);
+
+                await collection.FindOneAndUpdateAsync(filter, update);
+                await FollowupAsync($"Updated max warns for server: `{Context.Guild.Name}` to `{limit}`");
+                return;
+            }
+
+            await collection.InsertOneAsync(new WarnCollection.MaxWarnsObject
+            {
+                GuildId = Context.Guild.Id.ToString(),
+                MaxWarns = limit
+            });
+            await FollowupAsync($"Updated max warns for server: `{Context.Guild.Name}` to `{limit}`");
+        }
+    }
+}
